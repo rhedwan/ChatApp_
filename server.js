f159026c7b8ddfe6f96d109860dafec3e808cf22
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-06-06 21:12:37 +0100

    fix

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ io.on("connection", (socket) => {
     io.emit("message", "A user has left the chat");
   });
 });
-const PORT = 3000 || process.env.PORT;
+
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
